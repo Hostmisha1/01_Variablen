328b8c9dcde4e0e194de5a38f497739299c2d5e1
Misha <host.misha@googlemail.com>
2021-07-28 13:53:56 +0200

    Datentypen

--- script_01.js
+++ script_01.js
@@ -8,7 +8,33 @@
  */
 
 // Ausgabe
-console.log("Hallo Welt!");
-console.log(firstName);
+// console.log("Hallo Welt!");
+// //console.log(firstName); // nicht deklarierte Var. --> Fehler
+
+// 02 Deklaration + Wertzuweisung
+
+// let firstName;      // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+// let familyName = "Mütze"; // Deklaration und Wertzuweisung
+// console.log(familyName); // Ausgabe
+// console.log(firstName + " " + familyName); //Ausgabe
+
+//   // 03 Deklaration + Wertzuweisung II
+
+// let firstName,familyName;
+// firstName = prompt("Bitte Vornamen eingeben");
+// familyName = prompt("Bitte Nachnamen eingeben");
+// console.log(firstName); // Ausgabe
+// console.log(familyName); //Ausgabe
+// console.log("Hallo " + firstName + " " + familyName +"!"); //Ausgabe
 
 
+let test; // Deklaration (Definition)
+test = "Hi"; // string
+test = 2;  // number
+test = true; // boolean
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
